1831fa0e7bd9d2fcc02b4af3a74d0299c5169e8f
Samantha Greenwell Revita-Wagner <samgreenrevita@gmail.com>
2026-02-16 16:39:03 -0700

    Update run_benchmark.py

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,3 +1,6 @@
+# Copyright 2026 Samantha Revita-Wagner
+# Licensed under the Apache License, Version 2.0
+
 import asyncio
 from harness.traffic_simulator import TrafficSimulator
 from workloads.refactor_loop import RefactorLoop
